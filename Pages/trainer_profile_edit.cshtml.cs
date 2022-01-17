20d491de38239c8567c4837194baa2a32ea842e1
Xinlong She <19816542@student.westernsydney.edu.au>
2022-01-18 09:37:34 +1100

    trainer edit

---
+++ Pages/trainer_profile_edit.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Test_4._0.Pages
+{
+    public class trainer_profile_editModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
